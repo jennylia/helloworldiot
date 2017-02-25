f5afb6aa0b73601c0f9854465d870acb38856586
Jenny Lian <jennylian100@gmail.com>
2017-02-25 12:40:21 -0800

    try on raspberrypi

--- button.js
+++ button.js
@@ -1,38 +1,57 @@
 var five = require("johnny-five"),
-  board, button;
+  board;
+var firebase = require("firebase");
+//var myFirebaseRef = new Firebase("https://iothelloworld-ffb54.firebaseio.com/");
+
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+  apiKey: "AIzaSyAgmgGEgw5XbbdGnOfJirGWmeEbOZRjZM8",
+  authDomain: "iothelloworld-ffb54.firebaseapp.com",
+  databaseURL: "https://iothelloworld-ffb54.firebaseio.com",
+  storageBucket: "iothelloworld-ffb54.appspot.com",
+  messagingSenderId: "1027433377969"
+};
+firebase.initializeApp(config);
+
 
 board = new five.Board();
+
+var firebaseRef = firebase.database().ref();
 
 board.on("ready", function() {
 
   // Create a new `button` hardware instance.
   // This example allows the button module to
   // create a completely default instance
-  button = new five.Button(2);
+  var button = new five.Button(2);
+  var led = new five.Led(12);
 
-  // Inject the `button` hardware into
-  // the Repl instance's context;
-  // allows direct command line access
-  board.repl.inject({
-    button: button
-  });
 
   // Button Event API
 
   // "down" the button is pressed
   button.on("down", function() {
-    console.log("down");
+    console.log("in the future there is only war, down");
+    firebaseRef.child("button").set("down");
+    firebaseRef.child("johnny").set("hello");
   });
 
-  // "hold" the button is pressed for specified time.
-  //        defaults to 500ms (1/2 second)
-  //        set
-  button.on("hold", function() {
-    console.log("hold");
-  });
+
 
   // "up" the button is released
   button.on("up", function() {
-    console.log("up");
+    firebaseRef.child("button").set("up");
+    firebaseRef.child("johnny").set("bye");
+    led.off();
+  });
+
+  led.on();
+  firebaseRef.child("button").on("value", function(snap) {
+    if(snap.val() == "down") {
+      led.on();
+    } else {
+      led.off();
+    }
   });
 });
